337452f9d888463927289d4e8bc699145629a854
Askhab39 <Abdrazakov393@gmail.com>
2022-12-08 12:04:55 +0300

    доработал бронь и текст меню с framer

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import "./App.scss";
 import AppMain from "./MainMenu/AppMain";
 import Window from "./Authorization/AuthorizationWindow/Window";
 import Entry from "./Authorization/EntryWindow/Entry";
-import Modal from "./OpenModal/Modal";
-
-import AppMain from './MainMenu/AppMain';
 import { Routes, Route } from 'react-router-dom';
 import ProductPage from './pages/ProductPage';
+
 function App() {
   return (
     <div className="app">
