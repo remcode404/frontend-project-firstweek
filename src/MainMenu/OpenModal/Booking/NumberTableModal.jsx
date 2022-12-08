ea6013db7c6d2b589514c0892d161293db2cc340
Blacksider1 <adamtsurov123@gmail.com>
2022-12-08 11:57:27 +0300

    Сделал фронт для столиков

--- src/MainMenu/OpenModal/Booking/NumberTableModal.jsx
+++ src/MainMenu/OpenModal/Booking/NumberTableModal.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
-import style from './numberTableModal.scss'
+import style from './numberTableModal.module.scss';
 
-const NumberTableModal = () => {
+const NumberTableModal = ({ setNumberTable, setOpenNumberWindow }) => {
+  const handleAddNumber = (number) => {
+    setNumberTable(number)
+  };
+  
+  const handleCloseWindowTable = () => {
+    setOpenNumberWindow(false);
+  }
+
+  const showTable = () => {
+    const arrNumber = [];
+    for (let i = 0; i < 9; i++) {
+      arrNumber.push(i);
+    }
+    return arrNumber.map((number) => (
+      <div key={number} className={style.table}>
+        <div className={style.table_square}>
+          <div className={style.circle} onClick={() => handleAddNumber(number + 1)}>
+            {number + 1}
+          </div>
+        </div>
+      </div>
+    ));
+  };
+
   return (
-    <div></div>
+    <div className={style.number_table}>
+      <div className={style.number_text_button}>
+        <p className={style.number_text}>Номер столика: </p>
+        <button className={style.number_button} onClick={handleCloseWindowTable}>x</button>
+      </div>
+      <div className={style.divNT}>{showTable()}</div>
+    </div>
   );
 };
 
--- src/MainMenu/OpenModal/Modal.jsx
+++ src/MainMenu/OpenModal/Modal.jsx
@@ -5,27 +5,38 @@ import logo from './filesModal/logo.png';
 import Booking from './Booking/DateTimeSelect';
 import { useDispatch, useSelector } from 'react-redux';
 import { addBooking, fetchBooking } from '../../reducers/Slice/bookingSlice';
+import NumberTableModal from './Booking/NumberTableModal';
 
 function ModalWindow({ setModalWindow, modalWindow }) {
   const [openWindowDate, setOpenWindowDate] = useState(false);
+  const [openNumberWindow, setOpenNumberWindow] = useState(false);
   const [dataBooking, setDataBooking] = useState({});
-  const [name, setName] = useState('')
-  const [number, setNumber] = useState('')
-  const [dateTime, setDateTime] = useState({})
-  const [numberTable, setNumberTable] = useState(1)
-  
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [dateTime, setDateTime] = useState({});
+  const [numberTable, setNumberTable] = useState(1);
+
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-    dispatch(fetchBooking())
-    if(true){
-      setDataBooking({ numberTable: '1', nameUser: name, numberUser: number, date: dateTime.date, time: dateTime.time})
+  useEffect(() => {
+    dispatch(fetchBooking());
+    if (true) {
+      setDataBooking({
+        numberTable: numberTable,
+        nameUser: name,
+        numberUser: number,
+        date: dateTime.date,
+        time: dateTime.time,
+      });
     }
-  }, [dispatch, dateTime, name, number])
-
+  }, [dispatch, dateTime, name, number]);
 
   const handleOpenWindowDate = () => {
     setOpenWindowDate(true);
+  };
+
+  const handleOpenWindowNumber = () => {
+    setOpenNumberWindow(true);
   };
 
   const handleOpenWindow = () => {
@@ -33,20 +44,22 @@ function ModalWindow({ setModalWindow, modalWindow }) {
   };
 
   const handleAddBooking = () => {
-    if(true){
-      dispatch(addBooking(dataBooking))
+    if (true) {
+      dispatch(addBooking(dataBooking));
     }
-    
+
     console.log(dataBooking);
-  }
+  };
+
+  console.log(numberTable);
 
   const handleChangeName = (e) => {
-    setName(e.target.value)
-  }
+    setName(e.target.value);
+  };
 
   const handleChangePhone = (e) => {
-    setNumber(e.target.value)
-  }
+    setNumber(e.target.value);
+  };
 
   return (
     <div className={styles.parentModal}>
@@ -66,30 +79,44 @@ function ModalWindow({ setModalWindow, modalWindow }) {
             </div>
 
             <div className={styles.divInpName}>
-              <input type="text" placeholder=" Имя" onChange={handleChangeName} className={styles.inpName} />
+              <input
+                type="text"
+                placeholder=" Имя"
+                onChange={handleChangeName}
+                className={styles.inpName}
+              />
             </div>
 
             <div className={styles.divInpPhone}>
-              <input type="text" placeholder=" Телефон" onChange={handleChangePhone} className={styles.inpPhone} />
+              <input
+                type="text"
+                placeholder=" Телефон"
+                onChange={handleChangePhone}
+                className={styles.inpPhone}
+              />
             </div>
 
             <div className={styles.divBtnTableAndTime}>
-              <button className={styles.btnTable}>Столик</button>
+              <button className={styles.btnTable} onClick={handleOpenWindowNumber}>Столик</button>
+              {openNumberWindow && (
+                <div className={styles.divWindowNumber}>
+                  <NumberTableModal setNumberTable={setNumberTable} setOpenNumberWindow={setOpenNumberWindow}/>
+                </div>
+              )}
 
               <button className={styles.btnTime} onClick={handleOpenWindowDate}>
                 Время
               </button>
               {openWindowDate && (
                 <div className={styles.divWindowBooking}>
-                  <Booking
-                    setOpenWindowDate={setOpenWindowDate}
-                    setDateTime={setDateTime}
-                  />
+                  <Booking setOpenWindowDate={setOpenWindowDate} setDateTime={setDateTime} />
                 </div>
               )}
             </div>
             <div className={styles.divBtnBooking}>
-              <button className={styles.btnBooking} onClick={handleAddBooking}>Забронировать</button>
+              <button className={styles.btnBooking} onClick={handleAddBooking}>
+                Забронировать
+              </button>
             </div>
           </div>
         </div>
