e0ae833296d2df032bd87b6666606a7e23dc7da8
Askhab39 <Abdrazakov393@gmail.com>
2022-12-08 10:32:54 +0300

    Доработал окна регистрации, авторизации и бронирования с помощью Framer-motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.scss";
-// import AppMain from "./MainMenu/AppMain";
-// import Window from "./Authorization/AuthorizationWindow/Window";
-// import Entry from "./Authorization/EntryWindow/Entry";
+import AppMain from "./MainMenu/AppMain";
+import Window from "./Authorization/AuthorizationWindow/Window";
+import Entry from "./Authorization/EntryWindow/Entry";
 import Modal from "./OpenModal/Modal";
 
 function App() {
   return (
     <div className="app">
-      {/* <AppMain /> */}
-      {/* <Window /> */}
-      {/* <Entry /> */}
+      <AppMain />
+      <Window />
+      <Entry />
       <Modal />
     </div>
   );
